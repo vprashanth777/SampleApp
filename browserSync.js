55d604d99fb2c47ea191c5cf2d483d41840460cf
vprashanth777 <vprashanth777@yahoo.com>
2021-08-06 16:43:30 +0530

    Update and rename bs-config.js to browserSync.js

---
+++ browserSync.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const serve = require('browser-sync')
+const proxy = require('http-proxy-middleware')
+
+
+// browser sync settings
+serve({
+  port: 3000,
+  open: true,
+  files: [
+    'src/**/*',
+    {
+      match: ['**/*.html']
+    }
+  ],
+  reloadDelay: 3000,
+  proxy: {
+    target: 'dev.site',
+    middleware: bundler.middleware()
+  }
+})
+
+ui: false
